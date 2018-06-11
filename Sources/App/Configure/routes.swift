21a0de33b892325fa700b47b6ee915e0921ba92d
Danil <keepfocused@users.noreply.github.com>
2018-06-11 03:16:48 +0300

    New routes test

--- Sources/App/Configure/routes.swift
+++ Sources/App/Configure/routes.swift
@@ -14,4 +14,11 @@ public func routes(_ router: Router) throws {
         let msg = Message(id: UUID(), username: "@twostraws", content: "Hello, world!", date: Date())
         return msg
     }
+    
+    router.get("hello", String.parameter) { req -> String  in
+        
+        let name = try req.parameters.next(String.self)
+        
+        return "Hello, \(name)"
+    }
 }
